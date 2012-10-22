b552d4f9f349bee6728f8a6f2b6a0f52203815d3
Tisha Greenidge <tisha.m.greenidge@gmail.com>
2012-10-22 12:02:46 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaribTunes::Application.config.secret_token = '566798e4d640eda54d018d430ec4302200036f2ce3dc34d48b3ee280839c740da1efe019cd0a85f162761f9869cd13bcd6c3d63e08cad40a5abfcea76434d793'
